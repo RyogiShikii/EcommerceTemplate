a756dc8993711e9f254c24a2ef880e823550b00a
Daniel <guosongraiden@gmail.com>
2021-09-07 16:21:35 -0700

    initial setup

---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,15 @@
+import './menu-item.styles.scss';
+
+const MenuItem = (props) => {
+    return (
+        <div className={`menu-item ${props.size}`} >
+            <div className='background' style={{backgroundImage: `url(${props.imageUrl})`}}/>
+            <div className='content'>
+                <h1 className='title'>{props.title.toUpperCase()}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
