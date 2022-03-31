4e57075b03f3f48ae840c90b920887d20c0a0d81
Daniel <guosongraiden@gmail.com>
2022-03-31 16:31:47 -0700

    add router in collectionPreview

--- src/components/collection-preview/collectionPreview.js
+++ src/components/collection-preview/collectionPreview.js
@@ -1,11 +1,16 @@
 import './collectionpreview.styles.scss';
+import { withRouter } from 'react-router';
 
 import CollectionItem from '../collection-item/collectionItem';
 
 const CollectionPreview = (props) => {
     return (
         <div className='collection-preview'>
-            <h1 className='title'>{props.title.toUpperCase()}</h1>
+            <h1 className='title'
+                onClick={() => {props.history.push(`/shop/${props.title.toLowerCase()}`)}}
+            >
+                {props.title.toUpperCase()}
+            </h1>
             <div className='preview'>
                 {
                     props.items.filter((item,index) => index < 4).map( ({id,...otherItemnProps}) => {
@@ -17,4 +22,4 @@ const CollectionPreview = (props) => {
     )
 }
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
