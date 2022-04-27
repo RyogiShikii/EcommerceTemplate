7f84270c7e947952205eaa728e90505714f8b327
Daniel <guosongraiden@gmail.com>
2022-04-27 16:56:08 -0700

    update app.js
    change the way of how to subscribe the user state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import HomePage from './pages/homepage/homepage';
 import {Switch, Route} from 'react-router-dom';
 import Shop from './pages/shop/shop';
@@ -7,33 +7,29 @@ import Header from './components/header/Header';
 import SignInAndUp from './pages/sign-in-and-sign-up/signInAndUp';
 import CheckOut from './pages/checkoutpage/checkOutPage';
 
-import { onSnapshot } from 'firebase/firestore';
-import { createUserProfileDocument, auth } from './firebase/firebase.utils';
+import { onAuthStateChangedListener} from './firebase/firebase.utils';
+import { setCurrentUser } from './store/userSlice';
+import {useDispatch, useSelector} from 'react-redux';
 
 function App() {
-  const [user, setUser] = useState(null);
+  const dispatch = useDispatch();
+  const currentUser = useSelector(state => state.user.user)
 
-  /*useEffect(() => {
-    const subscribe = auth.onAuthStateChanged( async userAuth => {
-      if(userAuth){
-        const userRef = await createUserProfileDocument(userAuth);
-        const userSnap = onSnapshot(userRef, user => {
-          setUser({
-            id:user.id,
-            ...user.data()
-          })
-        })
+  useEffect(() => {
+    const subscribe = onAuthStateChangedListener((user) => {
+      if(user){
+        dispatch(setCurrentUser(user.accessToken));
       }else{
-        setUser(null);
+        dispatch(setCurrentUser(null))
       }
-    });
-    console.log('The current user is:',user)
+    })
+    console.log('The current user is:',currentUser)
     return subscribe;
-  },[auth])
-  */
+  },[currentUser])
+  
   return (
     <div>
-      <Header currentUser={user}/>
+      <Header currentUser={currentUser}/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
