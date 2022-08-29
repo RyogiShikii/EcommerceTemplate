77596e739d339399df4236e2f297b36857e9680f
Daniel <guosongraiden@gmail.com>
2022-08-29 16:11:32 -0700

    Add spinner according to shop status

--- src/components/collection-overview/collection-overview.js
+++ src/components/collection-overview/collection-overview.js
@@ -1,19 +1,21 @@
 import {CollectionOverviewContainer} from './collection-overview.styles.jsx';
+import Spinner from '../spinner/spinner';
 
 import { useSelector } from 'react-redux';
 import CollectionPreview from '../collection-preview/collectionPreview';
 
 const CollectionOverview = () => {
     const shopData = useSelector(state => state.shop.shop);
+    const isLoading = useSelector(state => state.shop.status);
     const items = Object.keys(shopData).map(key => {
         const data = shopData[key];
         return (
             <CollectionPreview key={key} title={key} items={data} />
         )
-    })
+    });
     return (
         <CollectionOverviewContainer>
-            {items}
+            {isLoading === 'loading'?<Spinner/>:items}
         </CollectionOverviewContainer>
     )
 }
--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,10 +1,12 @@
 import CollectionItem from "../../components/collection-item/collectionItem";
 import {CollectionContainer, Items} from './collection.styles.jsx';
+import Spinner from "../../components/spinner/spinner";
 
 import { useSelector } from "react-redux";
 
 const Collection = (props) => {
     const shop = useSelector(state => state.shop.shop);
+    const isLoading = useSelector(state => state.shop.status);
     //console.log(shop)
     const collections = shop[props.match.params.collectionId];
     //console.log(collections)
